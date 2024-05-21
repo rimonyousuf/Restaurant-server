7df0f54341d0a2bdcf55cd2822c24dac32a8b0fb
Rimon <rimonyousuf00@gmail.com>
2024-05-21 16:13:52 +0600

    Payment api has been added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -183,6 +184,20 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCOllection.deleteOne(query);
       res.send(result);
+    })
+
+    // Payment
+    app.post("/create-payment-intent",async(req,res)=>{
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
     // Send a ping to confirm a successful connection
